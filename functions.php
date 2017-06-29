6b26470690b2440bc4d8894ac30c66da6dbe1689
Ian Cole <ian.cole@gmail.com>
2017-06-29 11:52:12 +0000

    changed theme css load method, working to ensure that style.css is loaded AFTER bootstrap

--- functions.php
+++ functions.php
@@ -1,15 +1,9 @@
 <?php
 function my_theme_enqueue_styles() {
-
-    $parent_style = 'maker-faire-online-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+	wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css', 'bootstrap_css');
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 ?>
 
