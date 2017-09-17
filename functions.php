025c3c7c93fcbbaebee25b7548822f41c46f456c
Ryan P. Harrigan <rharrigan@findsomewinmore.com>
2017-09-17 10:27:11 -0400

    removed wp_enqueue of parent styles in functions.php. WP automatically adds parent styles.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
-	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css', 'bootstrap_css');
+	//wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css', 'bootstrap_css'); //removed because it is automatically included from parent - Ryan
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
